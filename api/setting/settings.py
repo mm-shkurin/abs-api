67acb3a60ae2fcbed76d2f3e7ac0fb7374d658be
mm-shkurin <trape3977@gmail.com>
2024-10-27 16:13:12 +0600

    fix static and media

--- api/setting/settings.py
+++ api/setting/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 
 from pathlib import Path
-
+import os
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -120,9 +120,14 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
-STATIC_URL = '/staticfiles/'
+STATIC_URL = '/static/'
 STATIC_ROOT = 'f:/absolute/api/staticfiles'
 
+# URL для доступа к медиа-файлам
+MEDIA_URL = '/media/'
+
+# Путь к директории, куда будут загружаться медиа-файлы
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')  # Убедитесь, что эта директория существует
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
